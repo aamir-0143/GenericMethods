bb17c898b5489f8453c07fdc13e5319ca566746a
Md aamir reza <aamirreza156@gamil.com>
2021-12-15 01:15:13 +0530

    [Aamir13] Add : Added Find The Max Number

---
+++ GenericMethods/MaximumNumber.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericMethods 
+{
+    public class MaximumNumber<T> where T : IComparable 
+    {
+
+        public T MaxMethod(T[] value)
+        {
+            var max = MaxValue(value);
+            return max;
+        }
+        public T[] Sort(T[] values)
+        {
+            Array.Sort(values);
+            return values;
+        }
+        public T MaxValue(T[] values)
+        {
+            var sorted_value = Sort(values);
+            return sorted_value[sorted_value.Length - 1];
+        }
+
+        public void PrintMaxValue(T[] value)
+        {
+            var max = MaxValue(value);
+            Console.WriteLine("Maximum value is: " + max);
+        }
+    }
+
+}
---
+++ GenericMethods/Program.cs
@@ -0,0 +1,15 @@
+﻿using GenericMethods;
+
+class program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Find Maximum Number!");
+
+        int[] intArray = { 1, 2, 9, 4 };
+        MaximumNumber<int> generic = new MaximumNumber<int>();
+        generic.PrintMaxValue(intArray);
+
+
+    }
+}
